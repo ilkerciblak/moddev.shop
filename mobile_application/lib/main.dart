a45096d04ff3d816b57525bd46d70897fe9b961e
ilkerciblak <ilkerciblak@gmail.com>
2024-12-15 15:08:47 +0300

    refactor(main): refactor main file imports

--- mobile_application/lib/main.dart
+++ mobile_application/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:mobile_application/common/_common.dart';
 import 'package:mobile_application/common/routing/app_routing.dart';
 import 'package:mobile_application/common/theme/input_decoration/input_decoration.dart';
 import 'package:mobile_application/feature/_feature.dart';
-import 'package:mobile_application/feature/user/presentation/user_route_registrar.dart';
 import 'package:mobile_application/presentation/_presentation.dart';
 
 Future<void> main() async {
